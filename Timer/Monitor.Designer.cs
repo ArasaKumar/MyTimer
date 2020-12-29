c910115708393c3902c6dc8dbba375a205674a38
Arasakumar Nachimuthu <arasakumarn@histogenetics.com>
2020-12-29 21:43:09 +0530

    Initial Commit

---
+++ Timer/Monitor.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Timer
+{
+    partial class Monitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxLog = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxLog
+            // 
+            this.tbxLog.Location = new System.Drawing.Point(0, 65);
+            this.tbxLog.Multiline = true;
+            this.tbxLog.Name = "tbxLog";
+            this.tbxLog.Size = new System.Drawing.Size(951, 516);
+            this.tbxLog.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(97, 31);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Monitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 593);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbxLog);
+            this.Name = "Monitor";
+            this.Text = "App Time Monitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbxLog;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Timer/Monitor.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Monitor : Form
+    {
+        public Monitor()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void RecordData()
+        {
+            Application.DoEvents();
+            using (FGHook obj = new FGHook())
+            {
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(new TimeSpan(0, 0, 1));
+                    tbxLog.Text = string.Join(Environment.NewLine, FGHook.APPList);
+                    Application.DoEvents();
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            RecordData();
+        }
+    }
+
+    #region Gater Data
+
+    class FGHook : IDisposable
+    {
+        // Delegate and imports from pinvoke.net:
+
+        public static List<string> APPList = new List<string>();
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr
+           hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
+           uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern Int32 GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        // Constants from winuser.h
+        const uint EVENT_SYSTEM_FOREGROUND = 3;
+        const uint WINEVENT_OUTOFCONTEXT = 0;
+        private IntPtr hhook;
+
+        // Need to ensure delegate is not collected while we're using it,
+        // storing it in a class field is simplest way to do this.
+        static WinEventDelegate procDelegate = new WinEventDelegate(WinEventProc);
+
+        public FGHook()
+        {
+            hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+
+        static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            GetForegroundProcessName();
+        }
+
+        static void GetForegroundProcessName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+
+            // The foreground window can be NULL in certain circumstances, 
+            // such as when a window is losing activation.
+            if (hwnd == null)
+                return;
+
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+            {
+                if (p.Id == pid)
+                {
+                    APPList.Add(p.ProcessName);
+                    return;
+                }
+            }
+            APPList.Add("Unknown");
+        }
+
+        public void Dispose()
+        {
+            UnhookWinEvent(hhook);
+        }
+
+        #endregion
+
+    }
+}
