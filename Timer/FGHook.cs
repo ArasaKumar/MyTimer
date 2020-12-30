379f42d93386548e062d0114c0cd9a4cf3d640f9
Arasakumar Nachimuthu <arasakumarn@histogenetics.com>
2020-12-30 14:37:00 +0530

    Moved the gather data to a new class

---
+++ Timer/FGHook.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+class FGHook : IDisposable
+{
+    // Delegate and imports from pinvoke.net:
+
+    public static List<string> APPList = new List<string>();
+
+    delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr
+       hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
+       uint idThread, uint dwFlags);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    static extern Int32 GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    [DllImport("user32.dll")]
+    static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+    // Constants from winuser.h
+    const uint EVENT_SYSTEM_FOREGROUND = 3;
+    const uint WINEVENT_OUTOFCONTEXT = 0;
+    private IntPtr hhook;
+
+    // Need to ensure delegate is not collected while we're using it,
+    // storing it in a class field is simplest way to do this.
+    static WinEventDelegate procDelegate = new WinEventDelegate(WinEventProc);
+
+    public FGHook()
+    {
+        hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+    }
+
+    static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+    {
+        GetForegroundProcessName();
+    }
+
+    static void GetForegroundProcessName()
+    {
+        IntPtr hwnd = GetForegroundWindow();
+
+        // The foreground window can be NULL in certain circumstances, 
+        // such as when a window is losing activation.
+        if (hwnd == null)
+            return;
+
+        uint pid;
+        GetWindowThreadProcessId(hwnd, out pid);
+
+        foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+        {
+            if (p.Id == pid)
+            {
+                APPList.Add(p.ProcessName);
+                return;
+            }
+        }
+        APPList.Add("Unknown");
+    }
+
+    public void Dispose()
+    {
+        UnhookWinEvent(hhook);
+    }
+
+}
