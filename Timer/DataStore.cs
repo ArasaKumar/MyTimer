668d78335e7b068b96d4493a512385902f1d0bd9
Arasakumar Nachimuthu <arasakumarn@histogenetics.com>
2020-12-30 21:35:58 +0530

    changes to monitor the focused window timings

--- Timer/DataStore.cs
+++ Timer/DataStore.cs
@@ -7,8 +7,9 @@ namespace Timer
     public class DataStore : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        private List<string> _lstAppData = new List<string>();
         private static DataStore _objDataStore = new DataStore();
+        private Dictionary<string, TimeSpan> _dicAppAndTime = new Dictionary<string, TimeSpan>();
+        private DateTime _dtmeFocusObtainedTime = DateTime.Now;
 
         public static DataStore GetInstance()
         {
@@ -24,14 +25,32 @@ namespace Timer
         {
             get
             {
-                return string.Join(Environment.NewLine, _lstAppData);
+                string strData = string.Empty;
+                foreach(KeyValuePair<string,TimeSpan> kv in _dicAppAndTime)
+                {
+                    strData += kv.Key + " : " + kv.Value + Environment.NewLine;
+                }
+                return strData;
             }
         }
 
         public void UpdateData(string pstrData)
         {
-            _lstAppData.Add(pstrData);
+            UpdateTimings(pstrData);
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("AppList"));
+            _dtmeFocusObtainedTime = DateTime.Now;
+        }
+
+        private void UpdateTimings (string pstrAppName)
+        {
+            if(_dicAppAndTime.ContainsKey(pstrAppName))
+            {
+                _dicAppAndTime[pstrAppName] = _dicAppAndTime[pstrAppName] + (DateTime.Now - _dtmeFocusObtainedTime);
+            }
+            else
+            {
+                _dicAppAndTime.Add(pstrAppName, (DateTime.Now - _dtmeFocusObtainedTime));
+            }
         }
     }
 }
