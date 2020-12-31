0cf02bc6df7d9997280e7bfcff09505365f38028
ArasakumarN <51780683+ArasakumarN@users.noreply.github.com>
2020-12-31 23:03:08 +0530

    Removed the unwanted usings

--- Timer/Focused Window Hook/FGHook.cs
+++ Timer/Focused Window Hook/FGHook.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using Timer;
 using Timer.Interfaces;
 
 class FGHook : IDisposable
--- Timer/SaveData/SaveToFile.cs
+++ Timer/SaveData/SaveToFile.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Timer.Interfaces;
 using System.IO;
 using System.Windows.Forms;
+using Timer.Interfaces;
 
 namespace Timer
 {
